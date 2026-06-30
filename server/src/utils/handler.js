e82c162e7655908ef9cec9107e962a562ac78885
Muhammad Ahmad Naveed <ahmadnaveedofficial05@gmail.com>
2026-06-30 18:22:27 +0500

    Update README.md

--- server/src/utils/handler.js
+++ server/src/utils/handler.js
@@ -7,3 +7,26 @@ const asyncHandler = (requestHandler) => {
 };
 
 export default asyncHandler;
+
+
+// import { NextRequest, NextResponse } from "next/server";
+
+// export function asyncHandler(
+//   handler: (req: NextRequest) => Promise<NextResponse>
+// ) {
+//   return async (req: NextRequest) => {
+//     try {
+//       return await handler(req);
+//     } catch (error: any) {
+//       return NextResponse.json(
+//         {
+//           success: false,
+//           message: error.message || "Internal Server Error",
+//         },
+//         {
+//           status: error.statusCode || 500,
+//         }
+//       );
+//     }
+//   };
+// }
